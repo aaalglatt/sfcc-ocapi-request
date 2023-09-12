b70ad4678c508eb1d72ff5bd1b8c0388e6ca1f03
geekhunger <hallo@geekhunger.de>
2023-09-12 12:39:32 +0200

    added more safty to the base url compilation helper

--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
-const {ENVIRONMENT_DOMAINS} = require("./credentials")
+const {ENVIRONMENT_DOMAINS, SUPPORTED_ENVIRONTMENTS, getSupportedEnvironment} = require("./credentials")
 const {join} = require("path")
 const helper = module.exports = {}
 
@@ -9,7 +9,18 @@ helper.protocol = function(value) {
 
 
 helper.api_base = function(environment, protocol) {
-    return ENVIRONMENT_DOMAINS[environment || "staging"].replace(/^(\w+\:\/+)?/i, helper.protocol(protocol))
+    let environment_fallback = false
+    let supported_environment = "staging" // default fallback environment
+    try {
+        supported_environment = getSupportedEnvironment(environment) // normalize the writing style because "dev" can be used to refer/match the "development" environment
+    } catch(_) {
+        environment_fallback = true
+    }
+    const base_url = ENVIRONMENT_DOMAINS[supported_environment].replace(/^(\w+\:\/+)?/i, helper.protocol(protocol))
+    if(environment_fallback === true) {
+        console.warn(`The Base URL ${JSON.stringify(base_url)} was compiled for the ${JSON.stringify(environment)} environment (a fallback) because the ${JSON.stringify(environment)} environment could not be found among supported environments ${JSON.stringify(SUPPORTED_ENVIRONTMENTS)}!`)
+    }
+    return base_url
 }
 
 
